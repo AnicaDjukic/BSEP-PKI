03d3a00f667a2c201835e4fe8ac541156c50cb2f
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2022-03-27 19:54:26 +0200

    fix: password encoder relocated

--- Bsep/src/main/java/com/Bsep/config/WebConfig.java
+++ Bsep/src/main/java/com/Bsep/config/WebConfig.java
@@ -1,16 +1,26 @@
 package com.Bsep.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
- 
-	// Za svrhe razvoja konfigurisemo dozvolu za CORS kako ne bismo morali @CrossOrigin anotaciju da koristimo nad svakim kontrolerom
+
+    // Za svrhe razvoja konfigurisemo dozvolu za CORS kako ne bismo morali @CrossOrigin anotaciju da koristimo nad svakim kontrolerom
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:8081").allowedOrigins("http://localhost:8080").allowedMethods("PUT", "DELETE", "GET", "POST");
     }
+
+    // Implementacija PasswordEncoder-a koriscenjem BCrypt hashing funkcije.
+    // BCrypt po defalt-u radi 10 rundi hesiranja prosledjene vrednosti.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
