ed2ac6d88c637855dfeb47dae74e972f9aeee402
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2022-03-27 17:56:49 +0200

    feat: bsep backend

---
+++ Bsep/src/main/java/com/Bsep/BsepApplication.java
@@ -0,0 +1,13 @@
+package com.Bsep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsepApplication.class, args);
+	}
+
+}
---
+++ Bsep/src/test/java/com/Bsep/BsepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bsep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
