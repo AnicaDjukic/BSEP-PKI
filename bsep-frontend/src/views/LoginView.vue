82e50234a27bcb626831875096b478ec1f4f9808
Kina999 <katarinazer6@gmail.com>
2022-03-29 19:33:24 +0200

    feat: login frontend started

---
+++ bsep-frontend/src/views/LoginView.vue
@@ -0,0 +1,68 @@
+<template>
+  <link
+    href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    rel="stylesheet"
+    id="bootstrap-css"
+  />
+  <div class="wrapper fadeInDown">
+    <div id="formContent">
+      <!-- Login Form -->
+      <form>
+        <input
+          type="text"
+          id="login"
+          class="fadeIn second"
+          name="login"
+          placeholder="login"
+          v-model="username"
+        />
+        <input
+          type="text"
+          id="password"
+          class="fadeIn third"
+          name="login"
+          placeholder="password"
+          v-model="password"
+        />
+        <input
+          type="submit"
+          class="fadeIn fourth"
+          value="Log In"
+          v-on:click="login()"
+        />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "LoginView",
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  mounted: function () {},
+  methods: {
+    login: function () {
+      axios
+        .post("http://localhost:8080/api/v1/auth/login", {
+          username: this.username,
+          password: this.password,
+        })
+        .then((response) => {
+          window.sessionStorage.setItem("jwt", response.data.jwt);
+          this.$router.push("/admin-page");
+        })
+        .catch((err) => {
+          alert(err.response.data);
+        });
+    },
+  },
+};
+</script>
+<style scoped src="@/css/Login.css"></style>
