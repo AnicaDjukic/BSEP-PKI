e3bbbf216abddf00e49ef1476690978fc54173ea
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2022-04-04 14:46:31 +0200

    feat: extensions choice started

--- bsep-frontend/src/views/AllCertificatesView.vue
+++ bsep-frontend/src/views/AllCertificatesView.vue
@@ -3,15 +3,34 @@
     <li><a class="active" href="/all-certificates-page">Certificates</a></li>
     <li><a href="/create-certificate-page">Create certificate</a></li>
   </ul>
+  <div v-for="c in certificates" :key="c" class="list-group">
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small>3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small>Donec id elit non mi porta.</small>
+  </a>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'AllCertificatesView',
   data: function () {
-    return {}
+    return {
+      certificates: []
+    }
   },
-  mounted: function () {},
+  mounted: function () {
+    axios
+      .get('http://localhost:8080/api/v1/certificate')
+      .then((response) => {
+        this.certificates = response.data
+      })
+  },
   methods: {}
 }
 </script>
